01c2e873d7244faf2d544cfe9865c6cf0d5c1740
BezYmienny <mirek.piotr@gmail.com>
2018-01-17 23:04:10 +0100

    3nd lesson - Tasks - startting a new task

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -10,6 +10,18 @@ namespace Tasks
     {
         static void Main(string[] args)
         {
+            Task t = Task.Run(() =>
+           {
+               for(int i=0;i<100;i++)
+               {
+                   Console.Write("*");
+               }
+               Console.WriteLine("!!!");
+           });
+
+            t.Wait();
+
+            Console.ReadKey();
         }
     }
 }
