6a0982779d4fe885e79f604c8722a903c4c8e24a
BezYmienny <mirek.piotr@gmail.com>
2018-01-19 13:40:19 +0100

    I agree - i don't understand cancelation of task

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Tasks
@@ -10,29 +11,67 @@ namespace Tasks
     {
         static void Main(string[] args)
         {
+            CancellationTokenSource source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
+
             Task<int> t = Task.Run(() =>
             {
-                return 42;
-            });
-            
-            
-            t.ContinueWith((i) =>
-            {
-                Console.WriteLine("Canceled");
+                //source.Cancel();
+                //token.
+                source.Cancel();
+                token.ThrowIfCancellationRequested();
                 
-            },TaskContinuationOptions.OnlyOnCanceled);
 
-            t.ContinueWith((i) =>
+                return 42;
+                //source.Cancel();
+            },token);
+            
+            
+            t.ContinueWith((i,j) =>
+            {
+                Console.WriteLine("@Canceled !");
+                
+            },TaskContinuationOptions.OnlyOnCanceled,token);
+
+            t.ContinueWith((i,j) =>
            {
                Console.WriteLine("Faulted");
-           }, TaskContinuationOptions.OnlyOnFaulted);
+           }, TaskContinuationOptions.OnlyOnFaulted,token);
+            
+           
+           
+                var completedTask = t.ContinueWith((i, j) =>
+               {
+                   try
+                   {
+                       Console.WriteLine("Completed");
+                   }
+                   catch
+                   {
+                   }
+               }, TaskContinuationOptions.OnlyOnRanToCompletion, token);
 
-        var completedTask =t.ContinueWith((i) =>
-      {
-          Console.WriteLine("Completed");
-      }, TaskContinuationOptions.OnlyOnRanToCompletion);
-        
-            completedTask.Wait();
+            Console.WriteLine("processing ....");
+            Console.ReadKey();
+            try
+            {
+                completedTask.Wait();
+            }
+            catch (AggregateException ae)
+            {
+                foreach (Exception e in ae.InnerExceptions)
+                {
+                    if (e is TaskCanceledException)
+                        Console.WriteLine("Unable to compute mean: {0}",
+                                          ((TaskCanceledException)e).Message);
+                    else
+                        Console.WriteLine("Exception: " + e.GetType().Name);
+                }
+            }
+            finally { }
+            {
+                source.Dispose();
+            }
             Console.WriteLine("hokus pokus");
             Console.ReadKey();
         }
